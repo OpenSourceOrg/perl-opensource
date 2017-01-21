3135bea1111863ad0679dde83245d715ed0a2741
Pablo Rodríguez González <pablo.rodriguez.gonzalez@gmail.com>
2017-01-22 00:54:46 +0100

    Ready for release

--- README.md
+++ README.md
@@ -1,10 +1,49 @@
 # NAME
 
-OpenSourceOrg::API - Perl API Client for OpenSource.org licenses API
+OpenSourceOrg::API - Perl API Bindings to the OSI License API 
 
 # VERSION
 
 version 0.001
+
+# SYNOPSIS
+
+```perl
+use OpenSourceOrg::API;
+my $client = OpenSourceOrg::API->new();
+my $all_licenses = $client->all();
+my $permisive_licenses = $client->tagged('permissive');
+my $mit = $client->get('MIT');
+my $Mozilla_2_0 = $client->get_by_scheme('SPDX', 'MPL-2.0');
+```
+
+# DESCRIPTION
+
+OpenSOurceOrg::API is an API Wrapper that allows you to query the Open Source License API with Perl.
+
+[https://github.com/OpenSourceOrg/api/blob/master/doc/endpoints.md](https://github.com/OpenSourceOrg/api/blob/master/doc/endpoints.md)
+
+# METHODS
+
+## all
+
+Get a list of all known licenses. 
+
+The response is the perl equivalent of the json returned by the api,
+documented in [https://github.com/OpenSourceOrg/api/blob/master/doc/endpoints.md#schema](https://github.com/OpenSourceOrg/api/blob/master/doc/endpoints.md#schema)
+
+## tagged
+
+Find all licenses tagged with a `keyword` as defined in 
+[https://github.com/OpenSourceOrg/api/blob/master/doc/endpoints.md#keywords](https://github.com/OpenSourceOrg/api/blob/master/doc/endpoints.md#keywords)
+
+## get
+
+Get a license by its OSI ID
+
+## get\_by\_scheme
+
+Get a license by its Scheme ID
 
 # AUTHOR
 
@@ -12,7 +51,7 @@ Pablo Rodríguez González <pablo.rodriguez.gonzalez@gmail.com>
 
 # COPYRIGHT AND LICENSE
 
-This software is Copyright (c) 2016 by Pablo Rodríguez González.
+Copyright (c) 2017 by Pablo Rodríguez González.
 
 This is free software, licensed under:
 
--- lib/OpenSourceOrg/API.pm
+++ lib/OpenSourceOrg/API.pm
@@ -6,12 +6,30 @@ use utf8;
 
 # VERSION
 
-# ABSTRACT: Perl API Client for OpenSource.org licenses API
+# ABSTRACT: Perl API Bindings to the OSI License API 
+
+=head1 SYNOPSIS
+
+    use OpenSourceOrg::API;
+    my $client = OpenSourceOrg::API->new();
+    my $all_licenses = $client->all();
+    my $permisive_licenses = $client->tagged('permissive');
+    my $mit = $client->get('MIT');
+    my $Mozilla_2_0 = $client->get_by_scheme('SPDX', 'MPL-2.0');
+
+=head1 DESCRIPTION
+
+OpenSOurceOrg::API is an API Wrapper that allows you to query the Open Source License API with Perl.
+
+L<https://github.com/OpenSourceOrg/api/blob/master/doc/endpoints.md>
+
+=cut
 
 use Moo;
 use REST::Client;
 use Const::Fast;
 use JSON;
+use Carp;
 
 const my $base_url => 'https://api.opensource.org';
 
@@ -25,23 +43,60 @@ sub _build__api_client {
     return $client;
 }
 
+=method all
+
+Get a list of all known licenses. 
+
+The response is the perl equivalent of the json returned by the api,
+documented in L<https://github.com/OpenSourceOrg/api/blob/master/doc/endpoints.md#schema>
+
+=cut
+
 sub all {
     my $self = shift();
     my $client = $self->_api_client;
     return $self->_handle_response( $client->GET('/licenses/') );
 }
 
-sub find {
+=method tagged
+
+Find all licenses tagged with a C<keyword> as defined in 
+L<https://github.com/OpenSourceOrg/api/blob/master/doc/endpoints.md#keywords>
+
+=cut
+
+sub tagged {
     my $self = shift();
     my $keyword = shift();
     my $client = $self->_api_client;
     return $self->_handle_response( $client->GET('/licenses/' . $keyword) );
 }
+
+=method get
+
+Get a license by its OSI ID
+
+=cut
+
 sub get {
     my $self = shift();
-    my $keyword = shift();
+    my $osi_id = shift();
     my $client = $self->_api_client;
-    return $self->_handle_response( $client->GET('/license/' . $keyword) );
+    return $self->_handle_response( $client->GET('/license/' . $osi_id) );
+}
+
+=method get_by_scheme
+
+Get a license by its Scheme ID
+
+=cut
+
+sub get_by_scheme {
+    my $self = shift();
+    my $scheme = shift();
+    my $id = shift();
+    my $client = $self->_api_client;
+    return $self->_handle_response( $client->GET('/license/' . join( '/', $scheme, $id ) ) );
 }
 
 sub _handle_response {
@@ -50,7 +105,8 @@ sub _handle_response {
     if ($response->responseCode() == 200) {
         return from_json($response->responseContent);
     } else {
-        return 'Error: ' . $response->responseCode() . ". Content: " . $response->responseContent ;
+        croak 'Error: ' . $response->responseCode() . ". Content: " . $response->responseContent ;
     }
 }
+
 1;
